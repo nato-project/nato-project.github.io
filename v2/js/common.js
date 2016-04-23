f97499d9cb884e4e11cb5f13e0cf1b0e01610068
Shivas Jayaram <shivasj@gmail.com>
2016-04-23 11:42:45 -0400

    Header update

--- v2/js/common.js
+++ v2/js/common.js
@@ -1,5 +1,30 @@
 
 // Load Header template
 $(function() {
-    $('#header').load('templates/header.html');
+
+    $('#header').load('templates/header.html', function() {
+
+        var page = document.location.pathname.match(/[^\/]+$/)[0];
+        console.log(page);
+
+        switch(page) {
+            case "index.html":
+                $('#index_page_menu').addClass("active");
+                break;
+            case "map.html":
+                $('#where_page_menu').addClass("active");
+                break;
+            case "what.html":
+                $('#what_page_menu').addClass("active");
+                break;
+            case "how.html":
+                $('#how_page_menu').addClass("active");
+                break;
+            default:
+                $('#index_page_menu').addClass("active");
+        }
+
+    });
+
+
 });
